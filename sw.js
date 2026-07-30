d8eeca98cd2d9d0eb9a174be664060e03cc13791
diegobarcoello-maker <diegobarcoello@gmail.com>
2026-07-30 10:21:54 -0500

    Cada leccion sube a 57-80 palabras, con dificultad creciente por nivel

--- sw.js
+++ sw.js
@@ -11,7 +11,7 @@
    petición que no sea a este mismo sitio.
    ============================================================ */
 
-const VERSION = 'speakup-v14';
+const VERSION = 'speakup-v15';
 
 const ESENCIALES = [
   './',
